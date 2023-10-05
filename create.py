6564fb98c69331cde6085f62d04ccebc8c53853f
Burhan Mohammad Sarfraz <burhan.sarfraz@bouvet.no>
2023-10-05 13:06:59 +0200

    Make git repo same as zip file

--- create.py
+++ create.py
@@ -1,14 +1,13 @@
 # powerpoint-karaoke-generator/1.0
 # chrifren@ifi.uio.no
 
+import datetime
+import json
 import re
 import sys
-import json
-import datetime
+
 import generator
 import pandoc
-
-
 
 #
 # Boot this thing
@@ -31,20 +30,15 @@ with open(pending_filename, "w") as fh:
     json.dump(s, fh)
 
 
-
-
-
 #
 # Grounding
-# 
+#
 
 grounding = """You are a writer producing a script for a speach. Based on the topic
    provided, select a title for the speach. Create five sections for the speach,
    each with three bullet points. Each bullet point should be a sentence or two.
-   Output the content in markdown format.
+   Output the content in markdown format. Alt skal skrives på norsk.
 """
-
-
 
 
 #
@@ -53,32 +47,56 @@ grounding = """You are a writer producing a script for a speach. Based on the to
 
 
 # title
-title = generator.complete("Create a title consisting of maximum 10 words for a speach about " + prompt, "")
+title = generator.complete(
+    "Lag en tittel som består av maksimalt 60 tegn for et foredrag om  " + prompt,
+    ", skriv på norsk. vær litt vulgær",
+)
 title.strip("\n\t\"")
 print("Title: " + title)
 
 # main topics
-topics = generator.complete("Based on the title \"{}\" create titles of max 10 words for five sections of the talk as a numbered list.".format(title))
+topics = generator.complete(
+    "basert på overskriften \"{}\"  lag overskrifter på maks 10 ord for 5 seksjoner av foredraget som en nummerert liste. ".format(
+        title
+    )
+)
 topics = re.sub(r"^\d+\.\s+", "", topics, flags=re.MULTILINE)
 
 
 # content of each topic
 p_stored = []
 n_stored = []
-for i, n in enumerate( topics.split("\n") ):
+for i, n in enumerate(topics.split("\n")):
     print("> Topic: " + n)
-    points = generator.complete("Based on the title \"{}\" create three bullet points of max 7 words for the section \"{}\", output as a numbered list.".format(title, n))
+    points = generator.complete(
+        "Based on the title \"{}\" create three bullet points of max 7 words for the section \"{}\", output as a numbered list.".format(
+            title, n
+        )
+    )
     points = re.sub(r"^\d+\.\s+", "", points, flags=re.MULTILINE)
     for n in points.split("\n"):
         print("  > " + n)
     p_stored.append(points)
 
     # create illustration
-    image_prompt = generator.complete("Based on the title \"{}\" create an prompt for DALLE to create a creative photo, 3d-art or illustration for the section \"{}\" where the main points are {}.".format(title, n, points), "")
-    generator.create_image(image_prompt, "presentations/img_{}_{}.png".format(id, i))
+    image_prompt = generator.complete(
+        "Based on the title \"{}\" create an prompt for DALLE to create a kids drawing for the section \"{}\" where the main points are {}.".format(
+            title, n, points
+        ),
+        " Max length of the prompt should be max 300 characters. ",
+    )
+    generator.create_image(
+        image_prompt + " there should be no text in the image.",
+        "presentations/img_{}_{}.png".format(id, i),
+    )
 
     # create speaker notes
-    notes = generator.complete("For a speach titled \"{}\", create a creative, witty and overly salesy narrative of maximum 100 words the section \"{}\" incorporating the main points: {}.".format(title, n, points), "")
+    notes = generator.complete(
+        "For a speach titled \"{}\", create a creative, witty and overly salesy narrative of maximum 100 words the section \"{}\" incorporating the main points: {}.".format(
+            title, n, points
+        ),
+        " svar på norsk",
+    )
     print("  > " + notes)
     n_stored.append(notes)
 
@@ -88,27 +106,26 @@ output_ppt = "presentations/{}.pptx".format(id)
 status_json = "presentations/{}.json".format(id)
 
 
-
-
-
 #
 # Generate the powerpoint doc
 #
 
 # save text to template.md
-date = datetime.datetime.now().strftime("%B %d, %Y") # format todays date as month name day, year
+date = datetime.datetime.now().strftime(
+    "%B %d, %Y"
+)  # format todays date as month name day, year
 with open(filename, "w") as fh:
     fh.write("% " + title + "\n")
     fh.write("% DeckDazzle/1.0" + "\n")
     fh.write("% " + date + "\n")
     fh.write("\n")
-    
-    for i, n in enumerate( topics.split("\n") ):
+
+    for i, n in enumerate(topics.split("\n")):
         fh.write("## " + n + "\n")
 
         fh.write(":::::::::::::: {.columns}\n")
         fh.write("::: {.column width=\"50%\"}\n")
-        
+
         for m in p_stored[i].split("\n"):
             fh.write("- " + m + "\n")
         fh.write("\n")
@@ -128,9 +145,6 @@ with open(filename, "w") as fh:
 
 # build powerpoint
 pandoc.run_docker_pandoc(filename, output_ppt)
-
-
-
 
 
 #
--- server.py
+++ server.py
@@ -1,13 +1,15 @@
-from flask import Flask, jsonify, request, abort, send_file, render_template
+#!usr/bin/env python3
 
-import os
+
 import json
-import copy
-import uuid
+import os
 import subprocess
-from datetime import datetime
+import uuid
+
+from flask import Flask, abort, jsonify, render_template, request, send_file
 
 app = Flask(__name__)
+
 
 @app.route("/api")
 def api():
@@ -19,29 +21,30 @@ def api():
                 "endpoint": "/presentations",
                 "method": "GET",
                 "format": "json",
-                "help": "Returns a list of all presentations."
+                "help": "Returns a list of all presentations.",
             },
-            "create-presentation": { 
-                "endpoint": "/create-presentation?q=<keyword>", 
-                "method": "POST", 
+            "create-presentation": {
+                "endpoint": "/create-presentation?q=<keyword>",
+                "method": "POST",
                 "format": "json",
-                "help": "Creates a new presentation with the given keyword."
+                "help": "Creates a new presentation with the given keyword.",
             },
             "presentation-status": {
                 "endpoint": "/presentations/<request_id>",
                 "method": "GET",
                 "format": "json",
-                "help": "Returns HTTP 202 until presentation is done, then 200 and json metadata."
+                "help": "Returns HTTP 202 until presentation is done, then 200 and json metadata.",
             },
             "download-presentation": {
                 "endpoint": "/presentations/<request_id>/download",
                 "method": "GET",
                 "format": "pptx",
-                "help": "Downloads the presentation with the given request id. Available after presentation-status returns 200."
-            }
-        }
+                "help": "Downloads the presentation with the given request id. Available after presentation-status returns 200.",
+            },
+        },
     }
     return jsonify(json)
+
 
 @app.route("/create-presentation", methods=["GET", "POST"])
 def create_presentation():
@@ -55,8 +58,23 @@ def create_presentation():
 
     # start karaoke.py in a subprocess with the keyword and the request id as params
     subprocess.Popen(["python3", "create.py", "\"{}\"".format(keyword), request_id])
-    
-    return jsonify({"q": keyword, "url": f"/presentations/{request_id}", "s": "pending"})
+
+    return jsonify(
+        {"q": keyword, "url": f"/presentations/{request_id}", "s": "pending"}
+    )
+
+
+@app.route("/alcohol", methods=["GET"])
+def alcohol():
+    # get alcohol value from sensor
+    value = request.args.get("v")
+    if value is None:
+        abort(400, "No alcohol value ")
+
+    print("alcohol value: " + value)
+
+    return jsonify({"alcohol": value})
+
 
 @app.route("/presentations", methods=["GET"])
 def list_presentations():
@@ -66,7 +84,13 @@ def list_presentations():
     # remove the file extension
     files = [f.split(".")[0] for f in files]
     # return unique list of files
-    presos = [{ "metadata" : f"/presentations/{u}", "source_url": f"/presentations/{u}/download" } for u in list(set(files))]
+    presos = [
+        {
+            "metadata": f"/presentations/{u}",
+            "source_url": f"/presentations/{u}/download",
+        }
+        for u in list(set(files))
+    ]
     # set status of each preso
     for p in presos:
         print(f"{p['metadata']}.json")
@@ -82,16 +106,16 @@ def list_presentations():
                 p['q'] = metadata["prompt"]
                 p['title'] = "TBD"
             p['status'] = "pending"
-        
+
     return jsonify(presos)
+
 
 @app.route("/presentations/<request_id>", methods=["GET"])
 def presentation_status(request_id):
     # check if the request_id is valid
     if request_id is None:
         abort(400, "Please provide a request id.")
-    
-    
+
     try:
         filename = "presentations/{}.json".format(request_id)
         with open(filename, "r") as fh:
@@ -99,7 +123,7 @@ def presentation_status(request_id):
             print(status)
             status['source_url'] = f"/presentations/{request_id}/download"
             return jsonify(status)
-    except:
+    except Exception:
         pass
 
     if os.path.exists(f"presentations/{request_id}.pending"):
@@ -116,12 +140,14 @@ def download_presentation(request_id):
     # send content of filename in result
     return send_file(filename, as_attachment=True)
 
+
 @app.route('/')
 def home():
     return render_template('index.html')
+
 
 if __name__ == "__main__":
     # make sub directory for presentations
     if not os.path.exists("presentations"):
         os.mkdir("presentations")
-    app.run(debug = True, host = "0.0.0.0", port = 3000)
+    app.run(debug=True, host="0.0.0.0", port=3000)
