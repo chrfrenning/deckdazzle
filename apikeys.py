a52f99e1827e2b9713586363c72a90232c30bb97
Burhan Mohammad Sarfraz <burhan.sarfraz@bouvet.no>
2023-10-09 21:22:47 +0200

    Removed old API keys

--- apikeys.py
+++ apikeys.py
@@ -1,2 +1,2 @@
-openai_api_key = "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"
-openai_organization = "org-RCtJ33QWo3DD9fvYLrVZAaRR"
+openai_api_key = "sk-..."
+openai_organization = "org-..."
