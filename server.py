3f9c1f391772b72ed11dba27232201af84c78d77
williamye-bouvet <143516392+williamye-bouvet@users.noreply.github.com>
2023-10-10 09:54:57 +0200

    Added Delete-all-Presentation function

--- server.py
+++ server.py
@@ -62,7 +62,21 @@ def create_presentation():
     return jsonify(
         {"q": keyword, "url": f"/presentations/{request_id}", "s": "pending"}
     )
-
+    
+@app.route("/delete-all-presentation", methods=["GET", "POST"])
+def delete_presentation():
+    folder = 'presentations'
+    for filename in os.listdir(folder):
+        file_path = os.path.join(folder, filename)
+        try:
+            if os.path.isfile(file_path) or os.path.islink(file_path):
+                os.unlink(file_path)
+            elif os.path.isdir(file_path):
+                shutil.rmtree(file_path)
+        except Exception as e:
+            print('Failed to delete %s. Reason: %s' % (file_path, e))
+            
+    return jsonify("OK")
 
 @app.route("/alcohol", methods=["GET"])
 def alcohol():
